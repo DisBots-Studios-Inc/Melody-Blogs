140a0b3dfc07432428c03ddfe84ad1982a181601
Aktindo <71206933+Aktindo@users.noreply.github.com>
2021-03-20 18:03:21 +0530

    added accent color

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,51 +1,69 @@
 module.exports = {
-  title: 'Melody Blogs',
-  description: 'Latest updates about the Melody Discord bot.',
-  base: '/',
-  dest: 'dist/',
-  theme: 'ououe',
+  title: "Melody Blogs",
+  description: "Latest updates about the Melody Discord bot.",
+  base: "/",
+  dest: "dist/",
+  theme: "ououe",
   themeConfig: {
-    cover: '/cover.jpg',
-    logo: '/logo.png',
+    cover: "/cover.jpg",
+    logo: "/logo.png",
     backgroundImage: false,
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Posts', link: '/posts/' },
-      { text: 'Tags', link: '/tag/' },
-      { text: 'Categories', link: '/category/' },
-      { text: 'About', link: '/about/' }
+      { text: "Home", link: "/" },
+      { text: "Posts", link: "/posts/" },
+      { text: "Tags", link: "/tag/" },
+      { text: "Categories", link: "/category/" },
+      { text: "About", link: "/about/" },
     ],
     footer: [
-      { text: 'Github', link: 'https://github.com/DisBots-Studios-Inc/Melody-Blogs' },
-      { text: 'Support Server', link: 'https://discord.gg/Fjrvtq4mZc' }
-    ]
+      {
+        text: "Github",
+        link: "https://github.com/DisBots-Studios-Inc/Melody-Blogs",
+      },
+      { text: "Support Server", link: "https://discord.gg/Fjrvtq4mZc" },
+    ],
   },
   plugins: [
     // permalink for posts
-    ['blog-multidir', {
-      postsDir: {
-        posts: 'posts/:year/:month/:day/:slug'
-      }
-    }],
+    [
+      "blog-multidir",
+      {
+        postsDir: {
+          posts: "posts/:year/:month/:day/:slug",
+        },
+      },
+    ],
     // add vuepress-plugin-container
-    ['container', {
-      type: 'right',
-      defaultTitle: ''
-    }],
-    ['container', {
-      type: 'tip',
-      before: info => `<div class="tip"><p class="title">${info}</p>`,
-      after: '</div>'
-    }],
-    ['container', {
-      type: 'warning',
-      before: info => `<div class="warning"><p class="title">${info}</p>`,
-      after: '</div>'
-    }],
-    ['container', {
-      type: 'danger',
-      before: info => `<div class="danger"><p class="title">${info}</p>`,
-      after: '</div>'
-    }]
-  ]
-}
+    [
+      "container",
+      {
+        type: "right",
+        defaultTitle: "",
+      },
+    ],
+    [
+      "container",
+      {
+        type: "tip",
+        before: (info) => `<div class="tip"><p class="title">${info}</p>`,
+        after: "</div>",
+      },
+    ],
+    [
+      "container",
+      {
+        type: "warning",
+        before: (info) => `<div class="warning"><p class="title">${info}</p>`,
+        after: "</div>",
+      },
+    ],
+    [
+      "container",
+      {
+        type: "danger",
+        before: (info) => `<div class="danger"><p class="title">${info}</p>`,
+        after: "</div>",
+      },
+    ],
+  ],
+};
